52a3961599c4b7ddf2a525cbef08409d014f96e1
mcgeq <mcgeq@outlook.com>
2025-12-22 17:30:23 +0800

    :sparkles: feat(uuid): Feat uuid

---
+++ src/morado/common/utils/uuid.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+UUID生成工具
+"""
+
+import string
+
+
+class UuidGenerator:
+    NUMERIC = string.digits
+    ALPHANUMERIC_LOWER = string.ascii_lowercase + string.digits
+
+
+if __name__ == "__main__":
+    pass
